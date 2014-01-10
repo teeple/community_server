394c88f9a064eb779a37660987bf2c571f96bdb3
newbiz <newbiz.uangel@gmail.com>
2014-01-10 12:02:17 +0900

    modify datepicker

--- app/assets/javascripts/custom_onready.js
+++ app/assets/javascripts/custom_onready.js
@@ -6,5 +6,6 @@
   $(document).ready(function () {
     //bind custom tab button click event
     //$("a[id^='tab_btn_']").bind('click', null, $.fn.custom_tab_click);
+    $('#datetimepicker').datetimepicker();
   });
 })(jQuery);
