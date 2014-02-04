811f20bc8a4fe7776c6f67e1955a14e84332e4a6
newbiz <newbiz.uangel@gmail.com>
2014-02-04 10:25:38 +0900

    multiple database in progress

--- app/models/sms_notification.rb
+++ app/models/sms_notification.rb
@@ -1,4 +1,5 @@
 class SmsNotification < ActiveRecord::Base
+	establish_connection "newbiz_noti"
 	belongs_to :user,  class_name: 'User',foreign_key: :receiver_user_id
 	belongs_to :event, class_name: 'Event'
 end
